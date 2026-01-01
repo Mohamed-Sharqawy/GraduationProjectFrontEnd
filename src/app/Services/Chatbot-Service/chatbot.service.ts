bdd33d9d1c91b34d862067cecb7c77e26037685d
Eng-Mohamed102 <mohamed4024@gmail.com>
2026-01-01 04:05:27 +0200

    mohamed_ayman_chatbot2

--- src/app/Services/Chatbot-Service/chatbot.service.ts
+++ src/app/Services/Chatbot-Service/chatbot.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environments';
 import { ChatbotAskRequest, ChatbotAskResponse, ChatbotWelcomeResponse } from '../../Models/Chatbot/chatbot.models';
+import { TranslationService } from '../Translation-Service/translation.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +13,23 @@ export class ChatbotService {
   // Using direct URL as requested by user instead of environment
   private apiUrl = `https://homyapi.runasp.net/api/chatbot`;
 
+  private translationService = inject(TranslationService);
+
   /**
    * Get welcome message
    */
   getWelcomeMessage(): Observable<ChatbotWelcomeResponse> {
-    return this.http.get<ChatbotWelcomeResponse>(`${this.apiUrl}/welcome`);
+    const lang = this.translationService.currentLang();
+    const headers = { 'Accept-Language': lang };
+    return this.http.get<ChatbotWelcomeResponse>(`${this.apiUrl}/welcome`, { headers });
   }
 
   /**
    * Send message to AI
    */
   ask(message: string): Observable<ChatbotAskResponse> {
-    return this.http.post<ChatbotAskResponse>(`${this.apiUrl}/ask`, { message });
+    const headers = { 'Content-Type': 'application/json' };
+    return this.http.post<ChatbotAskResponse>(`${this.apiUrl}/ask`, { Message: message }, { headers });
   }
 
   /**
